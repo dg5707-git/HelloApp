81c86c4e872f03406bab327ea8900d481158cdae
dg5707-git <dg5707@srmist.edu.in>
2026-03-26 12:47:05 +0530

    feat: printed hello using if loop

--- HelloApp.java
+++ HelloApp.java
@@ -3,9 +3,12 @@
 *@version:1.0
 */
 
-public class HelloApp_2{
- public static void main(String[] args){
-    String name = args[0];
-	System.out.println("Hello, " + name + "!");
-	}
+public class HelloApp_3{
+    public static void main(String[] args){
+        String name="World";
+        if(args.length>0){
+            name=args[0];
+       }
+       System.out.println("Hello, " +name+ "!");
+    }
 }
