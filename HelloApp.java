f6a8f9f20349294ebecc3f79d6dfedfe477746a6
dg5707-git <dg5707@srmist.edu.in>
2026-03-26 12:48:56 +0530

    feat:Ability to display many names

--- HelloApp.java
+++ HelloApp.java
@@ -3,12 +3,17 @@
 *@version:1.0
 */
 
-public class HelloApp_3{
-    public static void main(String[] args){
+public class HelloApp {
+    public static void main(String[] args) {
         String name="World";
-        if(args.length>0){
-            name=args[0];
-       }
-       System.out.println("Hello, " +name+ "!");
+        StringBuilder nameBuilder = new StringBuilder();
+        for(int i=0;i<args.length;i++){
+            nameBuilder.append(args[i]);
+            if(i<args.length-1){
+                nameBuilder.append(", ");
+            }
+            name = nameBuilder.toString();
+        }
+        System.out.println("Hello, " +name+ "!");
     }
 }
